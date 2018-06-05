1492fb4852873394c585afb9d0fd17ab185123df
igblee <gb_lee@126.com>
2018-06-05 23:41:53 +0800

    'init'

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,13 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import UploadPic from './UploadPic';
 
 export default class Main extends Component {
-  static propTypes = {
-    prop: PropTypes
-  }
-
   render() {
     return (
       <div className="main">
         <UploadPic />
       </div>
-    )
+    );
   }
 }
--- src/components/UploadPic.js
+++ src/components/UploadPic.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Webcam from 'react-webcam';
-import { Upload, Button, Icon } from 'antd';
+import { Upload, Button, Icon, Spin } from 'antd';
 import oAxios from '../config/Axios';
 
 export default class UploadPic extends Component {
@@ -11,6 +11,11 @@ export default class UploadPic extends Component {
     this.captureSome = this.captureSome.bind(this);
     this.uploadOne = this.uploadOne.bind(this);
     this.uploadSome = this.uploadSome.bind(this);
+    this.localUpload = this.localUpload.bind(this);
+    this.handleLocalUploadChange = this.handleLocalUploadChange.bind(this);
+    this.handleLocalUploadRemove = this.handleLocalUploadRemove.bind(this);
+    this.localUploadStatus = this.localUploadStatus.bind(this);
+
     this.state = {
       imageArr: [],
       imageOne: '',
@@ -18,6 +23,12 @@ export default class UploadPic extends Component {
       showSome: false,
       resultImg: '',
       resultGif: '',
+      localUploadStatus: false,
+      hasResult: false,
+      showBtn: false,
+      downloadStatus: true, // 不能点击
+      uploadStatus: false, // 可点击
+      otherBtnStatus: false,
     };
   }
 
@@ -31,11 +42,14 @@ export default class UploadPic extends Component {
       imageOne,
       showOne: true,
       showSome: false,
+      showBtn: true,
+      uploadStatus: false,
+      downloadStatus: true,
     });
   }
 
   captureSome() {
-    const times = 12;
+    const times = 24;
     const images = [];
     for (let i = 0; i < times; i++) {
       setTimeout(() => {
@@ -44,6 +58,9 @@ export default class UploadPic extends Component {
           imageArr: images,
           showSome: true,
           showOne: false,
+          showBtn: true,
+          uploadStatus: false,
+          downloadStatus: true,
         });
       }, 320 * i);
     }
@@ -52,6 +69,12 @@ export default class UploadPic extends Component {
   async uploadOne() {
     if (this.state.showOne) {
       const { imageOne } = this.state;
+      this.setState({
+        hasResult: true,
+        showOne: false,
+        uploadStatus: true,
+        otherBtnStatus: true,
+      });
       const data = {
         imageOne,
       };
@@ -63,12 +86,24 @@ export default class UploadPic extends Component {
           const { resultImg } = res;
           this.setState({
             resultImg,
+            hasResult: false,
+            downloadStatus: false,
+            otherBtnStatus: false,
           });
+        } else if (res.code === 401) {
+          alert(res.msg);
+          window.location.hash = '/login';
         } else {
           alert(res.msg);
         }
       } else {
         alert('计算失败，建议重新尝试！');
+        this.setState({
+          hasResult: false,
+          otherBtnStatus: false,
+          uploadStatus: false,
+          showOne: true,
+        });
       }
     }
   }
@@ -76,6 +111,12 @@ export default class UploadPic extends Component {
   async uploadSome() {
     if (this.state.showSome) {
       const { imageArr } = this.state;
+      this.setState({
+        hasResult: true,
+        showSome: false,
+        uploadStatus: true,
+        otherBtnStatus: true,
+      });
       const data = {
         imageArr,
       };
@@ -87,22 +128,55 @@ export default class UploadPic extends Component {
           const { resultGif } = res;
           this.setState({
             resultGif,
+            hasResult: false,
+            downloadStatus: false,
+            otherBtnStatus: false,
           });
+        } else if (res.code === 401) {
+          alert(res.msg);
+          window.location.hash = '/login';
         } else {
           alert(res.msg);
         }
       } else {
         alert('计算失败，建议重新尝试！');
+        this.setState({
+          hasResult: false,
+          otherBtnStatus: false,
+          uploadStatus: false,
+          showSome: true,
+        });
       }
     }
   }
 
+  localUpload() {
+
+  }
+
+  handleLocalUploadChange() {
+
+  }
+
+  handleLocalUploadRemove() {
+
+  }
+
+  localUploadStatus() {
+
+  }
   render() {
     const props = {
-      action: '//jsonplaceholder.typicode.com/posts/',
+      action: '',
       listType: 'picture',
-      multiple: true,
+      multiple: false,
       defaultFileList: '',
+      accept: 'image/jpg',
+      withCredentials: true,
+      customRequest: this.localUpload,
+      onChange: this.handleLocalUploadChange,
+      onRemove: this.handleLocalUploadRemove,
+      disabled: this.state.localUploadStatus,
     };
     const images = this.state.imageArr.map((item, index) => (
       <img src={item} key={index.toString()} alt="" />
@@ -113,7 +187,10 @@ export default class UploadPic extends Component {
           className="upload"
           {...props}
         >
-          <Button>
+          <Button
+            title="只能上传一张图片"
+            disabled={this.state.otherBtnStatus}
+          >
             <Icon type="upload" /> 本地上传
           </Button>
         </Upload>
@@ -122,6 +199,7 @@ export default class UploadPic extends Component {
             title="生成加了贴图的图片"
             className="btn"
             onClick={this.captureOne}
+            disabled={this.state.otherBtnStatus}
           >
             拍照
           </Button>
@@ -129,6 +207,7 @@ export default class UploadPic extends Component {
             title="连拍生成gif动图"
             className="btn"
             onClick={this.captureSome}
+            disabled={this.state.otherBtnStatus}
           >
             连拍
           </Button>
@@ -136,11 +215,55 @@ export default class UploadPic extends Component {
           <Webcam
             audio={false}
             height={300}
+            width={400}
             ref={this.setRef}
             screenshotFormat="image/jpeg"
-            width={400}
           />
         </div>
+        <div
+          className={
+            this.state.showBtn
+            ? 'upload-download-btn'
+            : 'hide'
+          }
+        >
+          <Button
+            className="result-btn-left"
+            disabled={this.state.uploadStatus}
+            onClick={
+              this.state.showSome
+              ? this.uploadSome
+              : this.uploadOne
+            }
+          >
+            是否上传
+          </Button>
+          <Button
+            className="result-btn-right"
+            disabled={this.state.downloadStatus}
+            onClick={
+              this.state.showSome
+              ? this.downloadSome
+              : this.downloadOne
+            }
+          >
+            点击下载
+          </Button>
+        </div>
+
+        <div
+          className={
+              this.state.hasResult
+              ? 'loading-container'
+              : 'hide'
+          }
+        >
+          <Spin
+            tip="疯狂计算中..."
+            className="spin"
+          />
+        </div>
+
         <div
           className={
             this.state.showOne
@@ -149,20 +272,8 @@ export default class UploadPic extends Component {
           }
         >
           <img src={this.state.imageOne} alt="" />
-          <br />
-          <Button
-            className="result-btn-left"
-            onClick={this.uploadOne}
-          >
-            是否上传
-          </Button>
-          <Button
-            className="result-btn-right"
-            onClick={this.downloadOne}
-          >
-            点击下载
-          </Button>
         </div>
+
         <div
           className={
             this.state.showSome
@@ -171,20 +282,8 @@ export default class UploadPic extends Component {
           }
         >
           {images}
-          <br />
-          <Button
-            className="result-btn-left"
-            onClick={this.uploadSome}
-          >
-            是否上传
-          </Button>
-          <Button
-            className="result-btn-right"
-            onClick={this.downloadSome}
-          >
-            点击下载
-          </Button>
         </div>
+
         <div
           className="show-result-img"
         >
